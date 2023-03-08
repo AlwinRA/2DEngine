fce5de38ee163a52432c10df3b85240b09217896
Alwin <alwinwithjesus@gmail.com>
2023-03-08 20:31:06 +0530

    ok

--- src/Game/game.cpp
+++ src/Game/game.cpp
@@ -27,11 +27,6 @@ void Game::Init()
     // load textures
     TextureResourceManager::LoadTexture("../Texture/Logo.png", true, "logo");
     Renderer->defaultTexture = TextureResourceManager::LoadTexture("../Texture/Logo.png", true, "logo");
-    // player->Sprite = TextureResourceManager::GetTexture("logo");
-    Entity obj(TextureResourceManager::GetTexture("logo"), glm::vec2(10.0f, 10.0f), glm::vec2(40.0f, 40.0f), 0.0f, glm::vec3(1.0f,1.0f,1.0f), glm::vec2(0.0f, 0.0f));
-    obj.IsSolid = true;
-    this->Entitys.push_back(obj);
-    player = new Entity(TextureResourceManager::GetTexture("logo"), glm::vec2(100.0f, 10.0f), glm::vec2(40.0f, 40.0f), 0.0f, glm::vec3(1.0f,0.0f,0.0f), glm::vec2(0.0f, 0.0f));
 }
 
 void Game::Update(float dt)
@@ -41,27 +36,8 @@ void Game::Update(float dt)
 
 void Game::ProcessInput(float dt)
 {
-    if (this->State == GAME_ACTIVE)
-    {
-        float velocity = 500 * dt;
-        // move playerboard
-        if (this->Keys[GLFW_KEY_A])
-        {
-            if (player->Position.x >= 0.0f)
-                player->Position.x -= velocity;
-        }
-        if (this->Keys[GLFW_KEY_D])
-        {
-            if (player->Position.x <= this->Width - player->Size.x)
-                player->Position.x += velocity;
-        }
-    }
 }
 
 void Game::Render()
 {
-    // Renderer->DrawSprite(glm::vec2(10.0f, 10.0f), glm::vec2(40.0f, 40.0f), 0.0f, glm::vec3(1.0f,1.0f,1.0f));
-    for (Entity&tile : this->Entitys)
-            tile.Draw(*Renderer);
-    player->Draw(*Renderer);
 }
